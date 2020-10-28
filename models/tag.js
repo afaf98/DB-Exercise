b8382546a1032df9ce34b36a6a66a046474925c2
Afaf Ibrahimi <afaf.ibrahimi@gmail.com>
2020-10-28 13:39:39 +0100

    New tag table and Itemtag table

--- models/tag.js
+++ models/tag.js
@@ -1,31 +1,28 @@
 "use strict";
 const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
-  class todoItem extends Model {
+  class tag extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      todoItem.belongsTo(models.todoList);
-      todoItem.belongToMany(models.tag, {
-        through: "id",
-        foreignKey: "todoItemId",
+      tag.belongsToMany(models.todoitem, {
+        through: "itemTags",
+        foreignKey: "tagId",
       });
     }
   }
-  todoItem.init(
+  tag.init(
     {
-      task: DataTypes.STRING,
-      deadline: DataTypes.STRING,
-      important: DataTypes.BOOLEAN,
-      todoListId: DataTypes.INTEGER,
+      title: DataTypes.STRING,
     },
     {
       sequelize,
-      modelName: "todoItem",
+      modelName: "tag",
     }
   );
-  return todoItem;
+  return tag;
 };
